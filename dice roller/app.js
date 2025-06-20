cfaf8be2765d03d45e0fecb1c1a22a81430b12de
itisrudraa <merudra01@gmail.com>
2025-06-20 20:26:32 +0530

    Initial commit with 3 web projects

---
+++ dice roller/app.js
@@ -0,0 +1,16 @@
+function rolldice(){
+    const numofdice = document.getElementById("numofdice").value;
+    const diceresult = document.getElementById("diceresult");
+    const diceimage = document.getElementById("diceimage");
+
+    const values = [];
+    const images = [];
+    for(let i=0; i<numofdice; i++){
+        const value = Math.floor(Math.random()*6) + 1;
+        values.push(value);
+        images.push(`<img src="dice_images/dice${value}.png" alt="dice ${value}">`);
+    }
+    
+    diceresult.textContent = `dice: ${values.join(', ')}`; 
+    diceimage.innerHTML = images.join('');
+}
